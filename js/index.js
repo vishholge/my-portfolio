8b3f887077e07085071e5457ee61fe2941b5ca79
vishwanath <vishwanathholge18@gmail.com>
2022-05-23 15:13:48 +0530

    added some js part

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 function mainProjects(){
     document.getElementById("mainproject").style.display = "block";
     document.getElementById("miniproject").style.display = "none";
+    document.getElementById("latestproject").style.display = "none";
     console.log("vishwanth");
     
 }
 function miniProjects(){
     document.getElementById("mainproject").style.display = "none";
     document.getElementById("miniproject").style.display = "block";
+    document.getElementById("latestproject").style.display = "none";
+}
+function latestProjects() {
+
+    document.getElementById("mainproject").style.display = "none";
+    document.getElementById("miniproject").style.display = "none";
+    document.getElementById("latestproject").style.display = "block";
 }
